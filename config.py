b7b625add0e0d07403caa2302cefc9682124ab27
Abhay Shihora <shihoraabhay@gmail.com>
2026-07-25 18:36:46 +0530

    Improve configuration defaults

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class Config:
     # Mail
    # Mail
     MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = int(os.getenv("MAIL_PORT"))
-    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
 
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")

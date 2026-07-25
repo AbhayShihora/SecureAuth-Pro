9bb686115a5165ea4da87e83c50992c57222003b
Abhay Shihora <shihoraabhay@gmail.com>
2026-07-25 20:18:42 +0530

    change on code

--- app/services/mail_service.py
+++ app/services/mail_service.py
@@ -4,35 +4,30 @@ from app.extensions import mail
 
 
 def send_otp_email(recipient_email, otp):
-    print("MAIL_SERVER:", current_app.config["MAIL_SERVER"])
-    print("MAIL_PORT:", current_app.config["MAIL_PORT"])
-    print("MAIL_USE_TLS:", current_app.config["MAIL_USE_TLS"])
-    print("Sending email...")
-    mail.send(msg)
-    print("Email sent!")
-    """msg = Message(
+
+    msg = Message(
         subject="SecureAuth Pro - Email Verification",
         sender=current_app.config["MAIL_DEFAULT_SENDER"],
         recipients=[recipient_email]
     )
 
     msg.body = f"""
-"""Hello,
+    Hello,
 
-Thank you for registering with SecureAuth Pro.
+    Thank you for registering with SecureAuth Pro.
 
-Your One-Time Password (OTP) is:
+    Your One-Time Password (OTP) is:
 
-========================
-        {otp}
-========================
+    ========================
+            {otp}
+    ========================
 
-This OTP is valid for 5 minutes.
+    This OTP is valid for 5 minutes.
 
-If you did not register, please ignore this email.
+    If you did not register, please ignore this email.
 
-Regards,
-SecureAuth Pro Team
-"""
+    Regards,
+    SecureAuth Pro Team
+    """
 
     mail.send(msg)

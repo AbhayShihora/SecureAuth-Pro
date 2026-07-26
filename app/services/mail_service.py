c4c23008eb5e9457ca3eef5c32217bb9f911df52
Abhay Shihora <shihoraabhay@gmail.com>
2026-07-26 14:19:21 +0530

    Mail send service

--- app/services/mail_service.py
+++ app/services/mail_service.py
@@ -1,7 +1,6 @@
 from flask_mail import Message
 from app.extensions import mail
 
-
 def send_otp_email(email, otp):
     print(f"Sending OTP to: {email}")
 
@@ -12,23 +11,12 @@ def send_otp_email(email, otp):
 
     msg.html = f"""
     <h2>Email Verification</h2>
-
-    <p>Hello,</p>
-
-    <p>Your OTP for email verification is:</p>
-
-    <h1 style="color:#2563eb;">{otp}</h1>
-
-    <p>This OTP will expire in <strong>10 minutes</strong>.</p>
-
-    <p>If you didn't request this OTP, you can safely ignore this email.</p>
-
-    <br>
-
-    <p>Regards,<br>
-    <strong>SecureAuth Pro Team</strong></p>
+    <h1>{otp}</h1>
     """
 
-    mail.send(msg)
-
-    print("✅ OTP email sent successfully.")
+    try:
+        mail.send(msg)
+        print("✅ OTP email sent successfully.")
+    except Exception as e:
+        print("❌ MAIL ERROR:", e)
+        raise
